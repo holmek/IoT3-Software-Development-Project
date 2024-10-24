446c2f7cd63a70614765ff19085d4f2d04874f65
Emil Holmgaard Kristoffersen <holm_e@icloud.com>
2024-10-24 18:21:37 +0000

    Initial commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, GitHub Actions!'
+    return 'Hello, GitHub Actions'
 
 if __name__ == '__main__':
     app.run(debug=True)
