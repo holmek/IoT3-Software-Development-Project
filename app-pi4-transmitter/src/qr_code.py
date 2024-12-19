549e02c86a1c28c5c6240bc54c1be464dfa45ae4
Emil Holmgaard <124199961+holmek@users.noreply.github.com>
2024-12-19 16:30:01 +0100

    Update qr_code.py

--- app-pi4-transmitter/src/qr_code.py
+++ app-pi4-transmitter/src/qr_code.py
@@ -6,14 +6,11 @@ import time
 buzzer = Buzzer(6)
 led = LED(26)
 
-
 def scan_qr_code_live():
     cap = cv2.VideoCapture(0)
     if not cap.isOpened():
-        print("Kan ikke åbne kameraet. Tjek forbindelsen.")
         return None
     detector = cv2.QRCodeDetector()
-    print("Søger efter QR-kode...")
 
     led.on()
 
@@ -31,7 +28,6 @@ def scan_qr_code_live():
                 led.off()
                 return data
             else:
-                print("QR-koden indeholder ikke et gyldigt CPR-nummer.")
                 buzzer.on()
                 time.sleep(0.1)
                 buzzer.off()
